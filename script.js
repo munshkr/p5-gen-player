7c55223ede1bf2810bbcd7a6b7a8c16789496806
Damián Silvani <munshkr@gmail.com>
2022-11-26 15:50:45 -0300

    feat: Set up MIDI

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const midiDeviceName = "Midi Through Port-0"
+const midiChannel = 1;
+
 // Create a new canvas to the browser size
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,3 +16,37 @@ function draw() {
   fill(255);
   rect(100, 200, 300, 300);
 }
+
+function init() {
+  // Enable WEBMIDI.js and trigger the onEnabled() function when ready
+  WebMidi
+    .enable()
+    .then(onEnabled)
+    .catch(err => alert(err));
+
+  // Function triggered when WEBMIDI.js is ready
+  function onEnabled() {
+    console.log(`${WebMidi.inputs.length} MIDI devices found:`);
+    WebMidi.inputs.forEach((device, i) => {
+      console.log(`[${i}] ${device.name}`)
+    })
+
+    const device = WebMidi.inputs.filter(device => device.name === midiDeviceName)[0];
+    if (!device) {
+      console.error(`No MIDI device named '${midiDeviceName}' was detected`)
+      return;
+    }
+
+    console.log(`Ready to listen on MIDI input '${midiDeviceName}' channel ${midiChannel}`)
+
+    device.channels[midiChannel].addListener("noteon", e => {
+      console.log("noteon", e.note.identifier, e.note.number, e.note.attack, e.note.release)
+    });
+
+    device.channels[midiChannel].addListener("noteoff", e => {
+      console.log("noteoff", e.note.identifier, e.note.number, e.note.attack, e.note.release)
+    });
+  }
+}
+
+init()
